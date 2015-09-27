78c13583c4b03b3d059912d864df0d7e3ff30144
Hardly Difficult <hardlydifficultllc@gmail.com>
2015-09-27 15:27:27 -0700

    Adding missing file.

---
+++ Hardly.Library.Twitch/Controller/TwitchUserPointManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Hardly.Games;
+
+namespace Hardly.Library.Twitch {
+	public class TwitchUserPointManager : PointManager {
+		readonly SqlTwitchUserPoints sqlPoints;
+
+		public TwitchUserPointManager(SqlTwitchChannel channel, SqlTwitchUser user) {
+			sqlPoints = new SqlTwitchUserPoints(user, channel);
+		}
+    
+        public override ulong AvailablePoints {
+            get {
+                GiveBonusIfTime();
+                return base.AvailablePoints;
+            }
+        }
+
+        public override ulong TotalPointsInAccount {
+            get {
+                return sqlPoints.points;
+            }
+            set {
+                sqlPoints.points = value;
+                sqlPoints.Save();
+            }
+        }
+
+        void GiveBonusIfTime() {
+			if(sqlPoints.points == 0 && (DateTime.Now - sqlPoints.timeOfLastBonus) > TimeSpan.FromMinutes(1)) {
+				sqlPoints.points = 420;
+				sqlPoints.timeOfLastBonus = DateTime.Now;
+				sqlPoints.Save();
+			}
+		}
+
+		
+	}
+}
