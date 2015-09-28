ec45e90dc8bd02fe7853fdacead7f7e537cc4565
1amnick <the1amnick@outlook.com>
2015-09-27 22:27:41 -0400

    you asked for it

--- Hardly.Library.Twitch.Chat/Commands/System/HelloCommand.cs
+++ Hardly.Library.Twitch.Chat/Commands/System/HelloCommand.cs
@@ -11,6 +11,7 @@ namespace Hardly.Library.Twitch.Commands.System {
             ChatCommand.Create(room, "hi", HiCommand, "Says hello", null, false, TimeSpan.FromSeconds(0), false);
             ChatCommand.Create(room, "echo", EchoCommand, "Echos what you say", null, false, TimeSpan.FromSeconds(0), false);
             ChatCommand.Create(room, "timemeout", TimeMeOut, "Shhh...", null, false, TimeSpan.FromSeconds(0), false);
+            ChatCommand.Create(room, "banme", BanYouBecauseYouAreStupidAndYouShouldntHaveDoneThat, "This will ban you. Dont do it unless you want to be benned.", null, false, TimeSpan.FromSeconds(0), false);
         }
 
         private void EchoCommand(SqlTwitchUser speaker, String additionalText) {
@@ -37,6 +38,12 @@ namespace Hardly.Library.Twitch.Commands.System {
                 //*do nothing ever i want this to be (somewhat)secret*//
             }
         }
+        private void BanYouBecauseYouAreStupidAndYouShouldntHaveDoneThat(SqlTwitchUser speaker, String lastWords) { //yay i changed my ide ti keep the brackets on this line!
+            room.SendChatMessage(".ban " + speaker.userName); //if this line gets ran you deserve it.
+            room.SendChatMessage(speaker.userName + " is now banned. Everyone in the chat should type 'F' or riPepperonis to pay respects."); //lol jk dont do this
+            lastWords = null; //noone cares 
+        }//Oh GOD do i feel dirty....
+
 
         private int randomTime(int maxTime) {
             SysRandom rnd = new SysRandom();
