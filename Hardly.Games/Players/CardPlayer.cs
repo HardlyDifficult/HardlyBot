b0749a28daef5b61d0d580dec9b43458c3b126af
Hardly Difficult <hardlydifficultllc@gmail.com>
2015-09-28 13:32:01 -0700

    Adding missing files.

---
+++ Hardly.Games/Players/CardPlayer.cs
@@ -0,0 +1,8 @@
+﻿namespace Hardly.Games {
+    public class CardPlayer<PlayerIdObjectType> : GamePlayer<PlayerIdObjectType> {
+        CardPlayerHand<PlayerIdObjectType> hand;
+
+        public CardPlayer(PointManager pointManager, PlayerIdObjectType playerObject) : base(pointManager, playerObject) {
+        }
+    }
+}
