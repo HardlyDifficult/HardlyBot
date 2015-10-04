33fbdf523779c0adf8b9eaa5b06cdaeaf10383f8
Hardly Difficult <hardlydifficultllc@gmail.com>
2015-10-04 12:04:29 -0700

    Added Uno project.

--- Hardly.Library.Twitch/Data/SqlTwitchBot.cs
+++ Hardly.Library.Twitch/Data/SqlTwitchBot.cs
@@ -1,61 +1,44 @@
-﻿using System.Diagnostics;
-
-namespace Hardly
-{
-    public class SqlTwitchBot : SqlRow
-    {
+﻿namespace Hardly {
+    public class SqlTwitchBot : SqlRow {
         public readonly SqlTwitchUser user;
 
         public SqlTwitchBot(SqlTwitchUser user, string oauthPassword = null)
-            : base(new object[] { user.id, oauthPassword })
-        {
+            : base(new object[] { user.id, oauthPassword }) {
             this.user = user;
         }
 
         internal static readonly SqlTable _table = new SqlTable("twitch_bots");
-        public override SqlTable table
-        {
-            get
-            {
+        public override SqlTable table {
+            get {
                 return _table;
             }
         }
 
-        uint userId
-        {
-            get
-            {
+        uint userId {
+            get {
                 return Get<uint>(0);
             }
         }
 
-        public string oauthPassword
-        {
-            get
-            {
+        public string oauthPassword {
+            get {
                 return Get<string>(1);
             }
-            set
-            {
+            set {
                 Set(1, value);
             }
         }
 
-        public static SqlTwitchBot[] GetAll()
-        {
+        public static SqlTwitchBot[] GetAll() {
             List<object[]> results = _table.Select(null, null, null, null, null, 0);
-            if (results != null)
-            {
+            if(results != null) {
                 SqlTwitchBot[] bots = new SqlTwitchBot[results.Count];
-                for (int i = 0; i < results.Count; i++)
-                {
+                for(int i = 0; i < results.Count; i++) {
                     bots[i] = new SqlTwitchBot(new SqlTwitchUser(results[i][0].FromSql<uint>()), results[i][1].FromSql<string>());
                 }
 
                 return bots;
-            }
-            else
-            {
+            } else {
                 return null;
             }
         }
